4c0a7fd7296a189b9e628a03efcceb836154f238
Adrian Sanjuan Plaza <adrsanpla@alu.edu.gva.es>
2025-02-14 16:58:50 +0100

    cambios en el login y en main

--- FrontEnd/src/Views/Login.jsx
+++ FrontEnd/src/Views/Login.jsx
@@ -5,17 +5,13 @@ import DivInput from '../Components/DivInput'
 import storage from '../Storage/storage'
 
 const Login = () => {
-  const [userName, setUsername] = useState('');
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('')
   const go = useNavigate();
-  const csrf = async () => {
-    await axios.get('/sanctum/csrf-cookie')
-  }
   const login = async (e) => {
     e.preventDefault();
-    await csrf();
-    const form = { userName: userName, password: password };
-    const res = await sendRequest('POST', form, '/api/auth/login', '', false);
+    const form = { userName: username, password: password };
+    const res = await sendRequest('POST', form, '/users/CSR/login', '', false);
     if (res.status == true) {
       storage.set('authToken', res.token);
       storage.set('authUser', res.data);
@@ -32,7 +28,7 @@ const Login = () => {
             </div>
             <div className='card-body'>
               <form onSubmit={login}>
-                <DivInput type='text' icon='fa-at' value={userName} className='form-control' placeholder='Username...' required='required' handleChange={(e) => setEmail(e.target.value)} />
+                <DivInput type='text' icon='fa-at' value={username} className='form-control' placeholder='Username...' required='required' handleChange={(e) => setUsername(e.target.value)} />
                 <DivInput type='password' icon='fa-key' value={password} className='form-control' placeholder='Password' required='required' handleChange={(e) => setPassword(e.target.value)} />
                 <div className='d-grid col-10 mx-auto'>
                   <button className='btn btn-dark'>
--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -6,18 +6,18 @@ import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 import axios from 'axios'
-let env = import.meta.env.base_url
-
+let host = import.meta.env.VITE_BASE_URL_BACKEND
+console.log(host)
 window.axios = axios
 
-window.axios.defaults.baseURL = env
+window.axios.defaults.baseURL = host
 window.axios.defaults.headers.common['Accept'] = 'application/json'
 window.axios.defaults.headers.common['Content-Type'] = 'application/json'
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 window.axios.defaults.withCredentials = true
 
+
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <App />
-  </StrictMode>,
 )
