655c18ae7abb4b20d6dd98e03b45a8010cd6fda0
Angel <angibaben@alu.edu.gva.es>
2025-02-16 21:14:08 +0100

    FormDep Funcional

--- FrontEnd/src/Components/FormDep.jsx
+++ FrontEnd/src/Components/FormDep.jsx
@@ -84,17 +84,21 @@ const FormDep = ({ id, title, Create = false , type}) => {
                             </div>
                             <div className='card-body'>
                                 <form onSubmit={save}>
-                                    <DivInput type='text' icon='fa-building' value={name} className='form-control' placeholder='Name' required ref={NameInput} handleChange={(e) => setName(e.target.value)} />
                                     {!Create ? (
                                         <>
+                                            <DivInput type='text' icon='fa-building' value={name} className='form-control' placeholder='Name' required ref={NameInput} handleChange={(e) => setName(e.target.value)} />
                                             <DivInput type='text' icon='fa-id-card' value={cif} className='form-control' placeholder='CIF' required handleChange={(e) => setCif(e.target.value)} />
                                             <DivInput type='text' icon='fa-user' value={personInCharge} className='form-control' placeholder='Person In Charge' required handleChange={(e) => setPersonInCharge(e.target.value)} />
                                             <DivInput type='text' icon='fa-id-card' value={personInChargeID} className='form-control' placeholder='Person In Charge ID' required handleChange={(e) => setPersonInChargeID(e.target.value)} />
                                         </>
                                     ) : (
                                         <>
+                                            <DivInput type='text' icon='fa-industry' value={cif} className='form-control' placeholder='Cif' handleChange={(e) => setCif(e.target.value)} />
+                                            <DivInput type='text' icon='fa-building' value={name} className='form-control' placeholder='Name' required ref={NameInput} handleChange={(e) => setName(e.target.value)} />
                                             <DivInput type='text' icon='fa-industry' value={type} className='form-control' placeholder='Type' handleChange={(e) => setType(e.target.value)} />
                                             <DivInput type='text' icon='fa-city' value={city} className='form-control' placeholder='City' required handleChange={(e) => setCity(e.target.value)} />
+                                            <DivInput type='text' icon='fa-industry' value={personInCharge} className='form-control' placeholder='setPersonInCharge' handleChange={(e) => setPersonInCharge(e.target.value)} />
+                                            <DivInput type='text' icon='fa-industry' value={personInChargeID} className='form-control' placeholder='setPersonInChargeID' handleChange={(e) => setPersonInChargeID(e.target.value)} />
                                             <DivInput type='text' icon='fa-map-marker-alt' value={address} className='form-control' placeholder='Address' handleChange={(e) => setAddress(e.target.value)} />
                                             <DivInput type='text' icon='fa-map' value={area} className='form-control' placeholder='Area' handleChange={(e) => setArea(e.target.value)} />
                                             <DivInput type='number' icon='fa-envelope' value={postalCode} className='form-control' placeholder='Postal Code' handleChange={(e) => setPostalCode(e.target.value)} />
@@ -117,13 +121,13 @@ const FormDep = ({ id, title, Create = false , type}) => {
     }else if (type == "Inscripcion"){
         return (
             <div>
-                <h1>Inscripciones</h1>
+                <h1>Inscripciones Crear</h1>
             </div>
         )
     }else if (type == "Usuarios"){
         return (
             <div>
-                <h1>Inscripciones</h1>
+                <h1>Usuarios Crear</h1>
             </div>
         )
 
